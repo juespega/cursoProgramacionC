0b2f66fdd20cbe216228bceb2e3502f5242b0acf
juespega <juespega666@gmail.com>
2023-03-16 00:27:16 -0500

    Estructura General de un Programa

---
+++ holaMundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//The # indicates that we are goning to import a library.
+//The "include" that we are going to include.
+/*<library>*/
+
+int main(){ //main function.
+    printf("Hello World!\n"); //The printf function is included in the <stadio.h> library.
+    return 0;//indicates that the program ended.
+}
